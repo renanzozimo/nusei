9a04f4ce0e3060eae0e59be166fbaf479e0929fe
Renan Zozimo <renanfz@gmail.com>
2021-09-04 17:09:40 -0300

    #0 - (feat/PROJ): start

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,10 +6,10 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   });
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nusei'`, () => {
+  it('should have as title \'nusei\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('nusei');
@@ -30,6 +30,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('nusei app is running!');
+    expect(compiled.querySelector('.content span').textContent)
+      .toContain('nusei app is running!');
   });
 });
